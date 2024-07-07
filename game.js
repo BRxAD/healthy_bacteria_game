5e4f330e0a438080282e2f3e5718e1ff74e3fe7f
BRxAD <51241393+BRxAD@users.noreply.github.com>
2024-07-06 20:40:18 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,3 @@
-const canvas = document.getElementById('gameCanvas');
-resizeCanvas();
-
-window.addEventListener('resize', resizeCanvas);
-
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-
-const ctx = canvas.getContext('2d');
-
 
 
 const canvas = document.getElementById('gameCanvas');
